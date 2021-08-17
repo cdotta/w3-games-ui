cfcb4db5cd9db7b452a825222aae5a2ca8f797a1
Cristian <cdotta@users.noreply.github.com>
2021-08-17 15:32:58 -0300

    Fix linter setup (#3)

--- components/CTAInfo.js
+++ components/CTAInfo.js
@@ -1,11 +1,15 @@
-import React from 'react';
 import { Box, Heading, Text } from '@chakra-ui/react';
+import React from 'react';
 
 export default function CTAInfo({ title, description }) {
   return (
     <Box>
-      <Heading size="2xl" mb="4">{title}</Heading>
-      <Text fontSize="xl" mb="8">{description}</Text>
+      <Heading size="2xl" mb="4">
+        {title}
+      </Heading>
+      <Text fontSize="xl" mb="8">
+        {description}
+      </Text>
     </Box>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import CTA from '../components/CTA';
 
 export default function Home() {
   return (
     <div>
-      <CTA title="Innovating gaming with the power of blockchain" description="Find out how you can be a part of the drive to advance web3 games and take your web3 game project to a whole new level" />
+      <CTA
+        title="Innovating gaming with the power of blockchain"
+        description="Find out how you can be a part of the drive to advance web3 games and take your web3 game project to a whole new level"
+      />
     </div>
   );
 }
