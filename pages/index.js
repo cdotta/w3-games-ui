c76abe06030183e018b34366f7b131514613f0e2
Caicobe Machado Lages <cmachadolages@gmail.com>
2021-08-17 12:18:25 -0300

    Create cta component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Heading, Text } from '@chakra-ui/react';
+import CTA from '../components/CTA';
 
 export default function Home() {
   return (
     <div>
-      <Heading>Innovating gaming with the power of blockchain</Heading>
-      <Text>
-        Find out how you can be a part of the drive to advance web3 games and
-        take your web3 game project to a whole new level
-      </Text>
+      <CTA title="Innovating gaming with the power of blockchain" description="Find out how you can be a part of the drive to advance web3 games and take your web3 game project to a whole new level" />
     </div>
   );
 }
