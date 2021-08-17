f77ac494b0967250f3aed5a698356316e784da32
Cristian Dotta <cdotta90@gmail.com>
2021-08-17 10:41:31 -0300

    Initial commit with font and pages setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+/* eslint-disable react/jsx-props-no-spreading */
+
+import '../styles/globals.css';
+import '@fontsource/nunito';
+import '@fontsource/exo';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../lib/theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
