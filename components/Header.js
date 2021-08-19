454a18c44a99356a5759eb784c74b0f7489ab42b
Cristian Dotta <cdotta90@gmail.com>
2021-08-18 22:43:54 -0300

    Header fit and finish

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,20 @@
-import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { Box, Flex, Stack, Text } from '@chakra-ui/react';
+import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const NAV_ITEMS = [
-  { label: 'Home', id: 1, route: '/' },
-  { label: 'Games', id: 2, route: '/games' },
-  { label: 'News', id: 3, route: '/news' },
-  { label: 'Incubate', id: 4, route: '/incubate' },
-  { label: 'About us', id: 5, route: '/about-us' },
+  { label: 'Home', route: '/' },
+  { label: 'Games', route: '/games' },
+  { label: 'News', route: '/news' },
+  { label: 'Incubate', route: '/incubate' },
+  { label: 'About us', route: '/about-us' },
 ];
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <Flex
       pl="10"
@@ -21,12 +25,31 @@ export default function Header() {
       overflow="hidden"
     >
       <Box pr="14">
-        <Image maxWidth="unset" src="/svgs/w3-logo.svg" alt="w3-logo" />
+        <Image width="40" height="37" src="/svgs/w3-logo.svg" alt="w3-logo" />
       </Box>
       {NAV_ITEMS.map((navItem) => (
-        <Link key={navItem.id} href={navItem.route} passHref>
+        <Link key={navItem.route} href={navItem.route} passHref>
           <Flex as="a" px="4" alignItems="center" height="100%">
-            <Text color="white">{navItem.label}</Text>
+            <Stack spacing="0.5" align="center">
+              <Text
+                color={
+                  router.asPath === navItem.route ? 'customOrange.500' : 'white'
+                }
+              >
+                {navItem.label}
+              </Text>
+              <Box
+                as="span"
+                h="6px"
+                w="6px"
+                borderRadius="50%"
+                bgColor={
+                  router.asPath === navItem.route
+                    ? 'customOrange.500'
+                    : 'transparent'
+                }
+              />
+            </Stack>
           </Flex>
         </Link>
       ))}
