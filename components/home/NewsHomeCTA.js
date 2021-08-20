4046ffa201391daa6ac5d99bcf37a48e9d6c2988
Cristian <cdotta@users.noreply.github.com>
2021-08-20 20:02:17 -0300

    Add missing news link (#19)

--- components/home/NewsHomeCTA.js
+++ components/home/NewsHomeCTA.js
@@ -1,8 +1,10 @@
-import { Box, HStack, Image, Text } from '@chakra-ui/react';
+import { Box, HStack, Image, Text, VStack } from '@chakra-ui/react';
+import { useRouter } from 'next/dist/client/router';
 import React from 'react';
 
 import Background from '../Background';
 import CTAInfo from '../CTAInfo';
+import { ArrowRightIcon } from '../icons';
 import VCTA from '../VCTA';
 
 const time = '5 min read';
@@ -35,6 +37,8 @@ const news = [
 ];
 
 export default function NewsHomeCTA() {
+  const router = useRouter();
+
   return (
     <VCTA
       background={<Background bgColor="black" />}
@@ -47,29 +51,41 @@ export default function NewsHomeCTA() {
         />
       }
       ctaContent={
-        <HStack spacing="8">
-          {news.map(
-            ({ imageUrl, imageAlt, time, title, description }, index) => (
-              <Box key={index} maxWidth="22rem">
-                <Image src={imageUrl} alt={imageAlt} />
-                <Box p="8" bgColor="warmBlack" width="100%">
-                  <Text color="white" fontWeight="semibold">
-                    <Text as="span" color="customOrange.500">
-                      GAMING
-                    </Text>{' '}
-                    - {time}
-                  </Text>
-                  <Text color="white" fontWeight="semibold" fontSize="2xl">
-                    {title}
-                  </Text>
-                  <Text color="white" noOfLines="3">
-                    {description}
-                  </Text>
+        <VStack spacing="4">
+          <HStack spacing="8">
+            {news.map(
+              ({ imageUrl, imageAlt, time, title, description }, index) => (
+                <Box key={index} maxWidth="22rem">
+                  <Image src={imageUrl} alt={imageAlt} />
+                  <Box p="8" bgColor="warmBlack" width="100%">
+                    <Text color="white" fontWeight="semibold">
+                      <Text as="span" color="customOrange.500">
+                        GAMING
+                      </Text>{' '}
+                      - {time}
+                    </Text>
+                    <Text color="white" fontWeight="semibold" fontSize="2xl">
+                      {title}
+                    </Text>
+                    <Text color="white" noOfLines="3">
+                      {description}
+                    </Text>
+                  </Box>
                 </Box>
-              </Box>
-            ),
-          )}
-        </HStack>
+              ),
+            )}
+          </HStack>
+          <HStack
+            width="100%"
+            justifyContent="flex-end"
+            alignItems="center"
+            onClick={() => router.push('/news')}
+            cursor="pointer"
+          >
+            <Text color="white">Read more news</Text>
+            <ArrowRightIcon />
+          </HStack>
+        </VStack>
       }
     />
   );
