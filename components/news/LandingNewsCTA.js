67d8f2a6bcc75a7b5304d487fc39dc7c2ba28c6c
Caicobe Machado Lages <cmachadolages@gmail.com>
2021-08-25 11:26:11 -0300

    Add landing news cta

---
+++ components/news/LandingNewsCTA.js
@@ -0,0 +1,136 @@
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  Spacer,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+
+import { BlackArrowRightIcon } from '../icons/index';
+
+export const topStories = [
+  {
+    text: 'Coder Drew Studios secures massive funding for their new blockbuster',
+  },
+  {
+    text: 'Dinox partners with W3 games to revolutionize the blockchain gaming space',
+  },
+  {
+    text: 'Will web 3 games overthrow regular online games anytime soon?',
+  },
+];
+
+export const trendingInfo = [
+  {
+    title: 'Amazing catchy title for the article',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    src: '/images/news/argos.png',
+    alt: 'argos',
+  },
+  {
+    title: 'Amazing catchy title for the article',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    src: '/images/news/trending.png',
+    alt: 'trending',
+  },
+  {
+    title: 'Amazing catchy title for the article',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    src: '/images/news/trending-consoles.png',
+    alt: 'trending consoles',
+  },
+];
+
+export default function LandingNewsCTA() {
+  return (
+    <VStack
+      background="black"
+      paddingX="10"
+      paddingY="20"
+      spacing="10"
+      alignItems="flex-start"
+    >
+      <Flex
+        justify="center"
+        width="100%"
+        justifyContent="flex-start"
+        alignItems="stretch"
+        flexDirection={['column', 'column', 'row']}
+      >
+        <Flex
+          w="container.lg"
+          h="lg"
+          mr="10"
+          backgroundImage={`linear-gradient(119.17deg, #F5F6F8 13.96%, rgba(245, 246, 248, 0) 67.05%), url('/images/news/game-of-the-season.png')`}
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+          backgroundSize="cover"
+        >
+          <VStack maxW="sm" pt={10} pl={10} alignItems="flex-start" spacing={4}>
+            <Heading>Most hype games of the season</Heading>
+            <HStack>
+              <Image
+                src="/images/news/coder-drew.png"
+                alt="coder drew"
+                fit="cover"
+                borderRadius="full"
+                boxSize="30px"
+              />
+              <Text>Coder Drew</Text>
+            </HStack>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna
+            </Text>
+          </VStack>
+        </Flex>
+        <Flex w="md" bgColor="white">
+          <Flex flexDirection="column" p="9">
+            <Heading as="h1" fontSize="xl" fontWeight="extrabold">
+              Top Stories
+            </Heading>
+            <Flex flexDirection="column" alignItems="center">
+              {topStories.map(({ text }, index) => (
+                <>
+                  <HStack spacing="4" py="7">
+                    <Text key={index} fontSize="lg">
+                      {text}
+                    </Text>
+                    <BlackArrowRightIcon />
+                  </HStack>
+                  <Divider orientation="horizontal" />
+                </>
+              ))}
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Flex w="100%" bgColor="white" height="72" flexDirection="column" p="10">
+        <Heading as="h1" fontSize="xl" fontWeight="extrabold">
+          Trending
+        </Heading>
+        <Flex>
+          {trendingInfo.map(({ src, alt, title, description }, index) => {
+            return (
+              <HStack spacing="4" key={index} py="12" pr="4">
+                <Image src={src} alt={alt} width="36" height="24" />
+                <Flex
+                  flexDirection="column"
+                  height="24"
+                  justifyContent="space-around"
+                >
+                  <Text fontWeight="bold">{title}</Text>
+                  <Text fontSize="sm">{description}</Text>
+                </Flex>
+              </HStack>
+            );
+          })}
+        </Flex>
+      </Flex>
+    </VStack>
+  );
+}
--- components/news/index.js
+++ components/news/index.js
@@ -1,3 +1,4 @@
+import LandingNewsCTA from './LandingNewsCTA';
 import NewsFeedSection from './NewsFeedSection';
 
-export const NewsCTAs = [NewsFeedSection];
+export const NewsCTAs = [LandingNewsCTA, NewsFeedSection];
