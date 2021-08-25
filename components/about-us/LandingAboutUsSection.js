5dfbd780dbdaa8acea24422a024f497c99176c5f
Cristian Dotta <cdotta90@gmail.com>
2021-08-25 19:42:00 -0300

    Add basic about-us section

---
+++ components/about-us/LandingAboutUsSection.js
@@ -0,0 +1,10 @@
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+
+export default function LandingHomeCTA() {
+  return (
+    <Box backgroundColor="black" minHeight="calc(100vh - 80px)">
+      <Text color="white">Prueba</Text>
+    </Box>
+  );
+}
