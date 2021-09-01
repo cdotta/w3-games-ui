60da681c90155e2e299bf0f1d322eec392758280
Cristian <cdotta@users.noreply.github.com>
2021-09-01 17:30:36 -0300

    Add hamburger menu header (#58)

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,18 @@
-import { Box, Button, Flex, Stack, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 
-import { UserIcon } from './icons';
+import { HamburgerIcon, UserIcon } from './icons';
 import MediaButtonsGroup from './MediaButtonsGroup';
 
 const NAV_ITEMS = [
@@ -16,11 +24,13 @@ const NAV_ITEMS = [
   { label: 'About us', route: '/about-us' },
 ];
 
+const customOrangeColor = 'customOrange.500';
+
 const NavItem = ({ navItem, isActive }) => (
   <Link href={navItem.route} passHref>
     <Flex as="a" px="4" alignItems="center" height="100%">
       <Stack spacing="0.5" align="center">
-        <Text color={isActive ? 'customOrange.500' : 'white'}>
+        <Text color={isActive ? customOrangeColor : 'white'}>
           {navItem.label}
         </Text>
         <Box
@@ -28,55 +38,136 @@ const NavItem = ({ navItem, isActive }) => (
           h="6px"
           w="6px"
           borderRadius="50%"
-          bgColor={isActive ? 'customOrange.500' : 'transparent'}
+          bgColor={isActive ? customOrangeColor : 'transparent'}
         />
       </Stack>
     </Flex>
   </Link>
 );
 
+export const mobileHeaderHeight = '64px';
+export const regularHeaderHeight = '80px';
+
 export default function Header() {
   const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <Flex
-      pl="10"
-      h="80px"
-      w="100%"
-      bgColor="warmBlack"
-      alignItems="center"
-      justifyContent="space-between"
-      overflow="hidden"
-    >
-      <Flex alignItems="center">
-        <Box pr="14" flexShrink="0">
-          <Image width="40" height="37" src="/svgs/w3-logo.svg" alt="w3 logo" />
-        </Box>
-        {NAV_ITEMS.map((navItem) => (
-          <Box
-            key={navItem.route}
-            display={['none', 'none', 'none', 'inherit']}
+    <Flex position="relative">
+      <Flex
+        pl={['6', '10']}
+        h={[mobileHeaderHeight, regularHeaderHeight]}
+        w="100%"
+        bgColor="warmBlack"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Flex alignItems="center">
+          <Flex pr="14" flexShrink="0">
+            <Image
+              width="40"
+              height="37"
+              src="/svgs/w3-logo.svg"
+              alt="w3 logo"
+            />
+          </Flex>
+          {NAV_ITEMS.map((navItem) => (
+            <Box
+              key={navItem.route}
+              display={['none', 'none', 'none', 'inherit']}
+            >
+              <NavItem
+                navItem={navItem}
+                isActive={router.asPath === navItem.route}
+              />
+            </Box>
+          ))}
+        </Flex>
+        <Flex
+          h="100%"
+          alignItems="center"
+          display={['none', 'none', 'none', 'flex']}
+        >
+          <MediaButtonsGroup mr="4" />
+          <Button
+            fontSize="md"
+            h="100%"
+            rightIcon={<UserIcon w={4} h={4} />}
+            borderRadius="0"
+            size="lg"
+            colorScheme="customOrange"
           >
-            <NavItem
-              navItem={navItem}
-              isActive={router.asPath === navItem.route}
+            My Account
+          </Button>
+        </Flex>
+        <Flex
+          h="100%"
+          alignItems="center"
+          display={['flex', 'flex', 'flex', 'none']}
+          position="relative"
+        >
+          <IconButton
+            h="100%"
+            borderRadius="0"
+            w={mobileHeaderHeight}
+            colorScheme="customBlack"
+            aria-label="My Account"
+            icon={<HamburgerIcon w="5" />}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          />
+          <IconButton
+            h="100%"
+            borderRadius="0"
+            w={mobileHeaderHeight}
+            colorScheme="customOrange"
+            aria-label="My Account"
+            icon={<UserIcon w="6" h="6" />}
+          />
+        </Flex>
+      </Flex>
+      {isMenuOpen && (
+        <Flex
+          backgroundColor="white"
+          top={[mobileHeaderHeight, regularHeaderHeight]}
+          w="48"
+          right="0"
+          position="absolute"
+          zIndex="docked"
+          display={['flex', 'flex', 'flex', 'none']}
+          flexDirection="column"
+        >
+          <Box position="relative">
+            <VStack align="start" paddingLeft="6" paddingY="6" spacing="4">
+              {NAV_ITEMS.map(({ label, route }, index) => {
+                const isActive = router.asPath === route;
+
+                return (
+                  <Link key={index} href={route} passHref>
+                    <Text
+                      as="a"
+                      color={isActive ? customOrangeColor : 'black'}
+                      borderRight={isActive ? '4px solid' : ''}
+                      width="100%"
+                      zIndex="dropdown"
+                    >
+                      {label}
+                    </Text>
+                  </Link>
+                );
+              })}
+            </VStack>
+            <MediaButtonsGroup alignSelf="center" color="black" />
+            <Box
+              position="absolute"
+              w="1"
+              h="100%"
+              background="gray.300"
+              top="0"
+              right="0"
             />
           </Box>
-        ))}
-      </Flex>
-      <Flex h="100%" alignItems="center" display={['none', 'none', 'flex']}>
-        <MediaButtonsGroup mr="4" />
-        <Button
-          fontSize="md"
-          h="100%"
-          rightIcon={<UserIcon w={4} h={4} />}
-          borderRadius="0"
-          size="lg"
-          colorScheme="customOrange"
-        >
-          My Account
-        </Button>
-      </Flex>
+        </Flex>
+      )}
     </Flex>
   );
 }
--- components/MediaButtonsGroup.js
+++ components/MediaButtonsGroup.js
@@ -3,27 +3,31 @@ import React from 'react';
 
 import { DiscordIcon, MediumIcon, TelegramIcon, TwitterIcon } from './icons';
 
-export default function MediaButtonsGroup(properties) {
+export default function MediaButtonsGroup({ color, ...properties }) {
   return (
     <HStack {...properties}>
       <IconButton
         aria-label="Telegram icon"
         icon={<TelegramIcon h={6} w={6} />}
+        color={color}
         colorScheme="transparent"
       />
       <IconButton
         aria-label="Discord icon"
         icon={<DiscordIcon h={6} w={6} />}
+        color={color}
         colorScheme="transparent"
       />
       <IconButton
         aria-label="Twitter icon"
         icon={<TwitterIcon h={6} w={6} />}
+        color={color}
         colorScheme="transparent"
       />
       <IconButton
         aria-label="medium icon"
         icon={<MediumIcon h={6} w={6} />}
+        color={color}
         colorScheme="transparent"
       />
     </HStack>
