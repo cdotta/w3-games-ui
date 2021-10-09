b55cdf0d1014ca87259341989c613f8bca7b4c1c
Cristian Dotta <cdotta90@gmail.com>
2021-10-09 08:43:59 -0300

    Add dynamic carousel games

--- pages/games/index.js
+++ pages/games/index.js
@@ -2,13 +2,31 @@ import React from 'react';
 
 import { GamesSections } from '../../components/games';
 import Page from '../../components/Page';
+import axios from '../../lib/cmsAxios';
 
-export default function Games() {
+export const GamesContext = React.createContext({});
+
+export default function Games(properties) {
   return (
-    <Page>
-      {GamesSections.map((GamesSection, index) => (
-        <GamesSection key={index} />
-      ))}
-    </Page>
+    <GamesContext.Provider value={properties}>
+      <Page>
+        {GamesSections.map((GamesSection, index) => (
+          <GamesSection key={index} />
+        ))}
+      </Page>
+    </GamesContext.Provider>
   );
+}
+
+// eslint-disable-next-line unicorn/prevent-abbreviations
+export async function getStaticProps() {
+  const [{ data: carouselGames }] = await Promise.all([
+    axios.get('carousel-games').catch(() => ({ data: [] })),
+  ]);
+
+  return {
+    props: {
+      carouselGames: carouselGames || null,
+    },
+  };
 }
